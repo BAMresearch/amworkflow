f4d9659ba0b82e25656a2a5e5dfa2ccccc6c3995
yuxiang <yuxianghe61@gmail.com>
2023-08-08 15:55:09 +0200

    try solution 5

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='amworkflow',
+    version='1.0',
+    packages=['.amworkflow'],
+    url='',
+    license='MIT',
+    author='Yuxiang He',
+    author_email='yuxiang.he@bam.de',
+    description=''
+)
