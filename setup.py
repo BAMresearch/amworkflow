8e5316496cf99a9063e104696559c9a8ac4da6f1
yuxiang <yuxianghe61@gmail.com>
2023-08-08 13:37:13 +0200

    still wrong. trying without running code first

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     name='amworkflow',
     version='1.0',
     packages=['amworkflow.src', 'amworkflow.tests'],
-    install_requires=[f"OCCUtils-0.1.dev0 @ file://localhost/{path_to_am}/amworkflow/dependencies/OCCUtils-0.1.dev0-py3-none-any.whl"],
-    package_data={
-        # Include the dependencies along with your package
-        'my_package': ['amworkflow.dependencies/*']
-    },
+    # install_requires=[f"OCCUtils-0.1.dev0 @ file://localhost/{path_to_am}/amworkflow/dependencies/OCCUtils-0.1.dev0-py3-none-any.whl"],
+    # package_data={
+    #     # Include the dependencies along with your package
+    #     'my_package': ['amworkflow.dependencies/*']
+    # },
     url='',
     license='MIT',
     author='Yuxiang He',
