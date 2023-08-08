690635daed8ddece72d659817e297cff200cbf09
yuxiang <yuxianghe61@gmail.com>
2023-08-08 14:04:49 +0200

    wrong. trying solution 2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ path_to_am = os.getcwd()
 setup(
     name='amworkflow',
     version='1.0',
-    packages=['amworkflow/src', 'amworkflow/tests'],
+    packages=['amworkflow'],
     # install_requires=[f"OCCUtils-0.1.dev0 @ file://localhost/{path_to_am}/amworkflow/dependencies/OCCUtils-0.1.dev0-py3-none-any.whl"],
     # package_data={
     #     # Include the dependencies along with your package
