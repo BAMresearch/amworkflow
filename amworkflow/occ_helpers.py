12a0ac7940300e5dc87172a60f606476ce7d5060
yuxiang <yuxianghe61@gmail.com>
2023-11-08 13:27:48 +0100

    try pass the trussarc

--- amworkflow/occ_helpers.py
+++ amworkflow/occ_helpers.py
@@ -4,19 +4,28 @@ from OCC.Core.BOPAlgo import BOPAlgo_Splitter
 from OCC.Core.BRep import BRep_Builder
 from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Cut, BRepAlgoAPI_Fuse
 from OCC.Core.BRepBndLib import brepbndlib_Add
-from OCC.Core.BRepBuilderAPI import (BRepBuilderAPI_Copy,
-                                     BRepBuilderAPI_MakeSolid,
-                                     BRepBuilderAPI_Sewing)
+from OCC.Core.BRepBuilderAPI import (
+    BRepBuilderAPI_Copy,
+    BRepBuilderAPI_MakeSolid,
+    BRepBuilderAPI_Sewing,
+)
 from OCC.Core.BRepGProp import brepgprop_SurfaceProperties
 from OCC.Core.gp import gp_Pln, gp_Pnt, gp_Trsf, gp_Vec
 from OCC.Core.GProp import GProp_GProps
-from OCC.Core.TopAbs import (TopAbs_COMPOUND, TopAbs_EDGE, TopAbs_FACE,
-                             TopAbs_SHELL, TopAbs_SOLID, TopAbs_WIRE)
+from OCC.Core.TopAbs import (
+    TopAbs_COMPOUND,
+    TopAbs_EDGE,
+    TopAbs_FACE,
+    TopAbs_SHELL,
+    TopAbs_SOLID,
+    TopAbs_WIRE,
+)
 from OCC.Core.TopExp import TopExp_Explorer
 from OCC.Core.TopLoc import TopLoc_Location
 from OCC.Core.TopoDS import TopoDS_Compound, TopoDS_Face, TopoDS_Shape
 from OCCUtils.Construct import make_face, vec_to_dir
 from OCCUtils.Topology import Topo
+
 from amworkflow.geometry import simple_geometries as sgeom
 
 
@@ -28,46 +37,44 @@ def sew_face(*component) -> TopoDS_Shape:
     sewed_shape = sewing.SewedShape()
     return sewed_shape
 
+
 def cutter3D(shape1: TopoDS_Shape, shape2: TopoDS_Shape) -> TopoDS_Shape:
     """
-     @brief Cut a TopoDS_Shape from shape1 by shape2. It is possible to use this function to cut an object in 3D
-     @param shape1 shape that is to be cut
-     @param shape2 shape that is to be cut. It is possible to use this function to cut an object in 3D
-     @return a shape that is the result of cutting shape1 by shape2 ( or None if shape1 and shape2 are equal
+    @brief Cut a TopoDS_Shape from shape1 by shape2. It is possible to use this function to cut an object in 3D
+    @param shape1 shape that is to be cut
+    @param shape2 shape that is to be cut. It is possible to use this function to cut an object in 3D
+    @return a shape that is the result of cutting shape1 by shape2 ( or None if shape1 and shape2 are equal
     """
     comm = BRepAlgoAPI_Cut(shape1, shape2)
     return comm.Shape()
 
 
-def translate(item: TopoDS_Shape,
-              vector: list):
+def translate(item: TopoDS_Shape, vector: list):
     """
     @brief Translates the shape by the distance and direction of a given vector.
     @param item The item to be translated. It must be a TopoDS_Shape
     @param vector The vector to translate the object by. The vector has to be a list with three elements
     """
     ts_handler = gp_Trsf()
-    ts_handler.SetTranslation(gp_Vec(vector[0],
-                                     vector[1],
-                                     vector[2]))
+    ts_handler.SetTranslation(gp_Vec(vector[0], vector[1], vector[2]))
     loc = TopLoc_Location(ts_handler)
     item.Move(loc)
 
 
 def reverse(item: TopoDS_Shape):
     """
-     @brief Reverse the shape.
-     @param item The item to reverse.
-     @return The reversed item
+    @brief Reverse the shape.
+    @param item The item to reverse.
+    @return The reversed item
     """
     return item.Reversed()
 
 
 def geom_copy(item: TopoDS_Shape):
     """
-     @brief Copy a geometry to a new shape. This is a wrapper around BRepBuilderAPI_Copy and can be used to create a copy of a geometry without having to re - create the geometry in the same way.
-     @param item Geometry to be copied.
-     @return New geometry that is a copy of the input geometry.
+    @brief Copy a geometry to a new shape. This is a wrapper around BRepBuilderAPI_Copy and can be used to create a copy of a geometry without having to re - create the geometry in the same way.
+    @param item Geometry to be copied.
+    @return New geometry that is a copy of the input geometry.
     """
     wire_top_builder = BRepBuilderAPI_Copy(item)
     wire_top_builder.Perform(item, True)
@@ -75,11 +82,13 @@ def geom_copy(item: TopoDS_Shape):
     return new_item
 
 
-def split(item: TopoDS_Shape,
-          nz: int = None,
-          layer_height: float = None,
-          nx: int = None,
-          ny: int = None):
+def split(
+    item: TopoDS_Shape,
+    nz: int = None,
+    layer_height: float = None,
+    nx: int = None,
+    ny: int = None,
+):
     """
     @brief Split a TopoDS_Shape into sub - shapes.
     @param item TopoDS_Shape to be split.
@@ -89,48 +98,55 @@ def split(item: TopoDS_Shape,
     @param ny Number of sub - shapes in the y - direction.
     @return a compound of sub-shapes
     """
-    assert [nz, layer_height].count(None) == 1
+    # assert [nz, layer_height].count(None) == 1
 
     xmin, ymin, zmin, xmax, ymax, zmax = get_occ_bounding_box(item)
     plan_len = 1.2 * max(abs(xmin - xmax), abs(ymin - ymax))
     z = zmax - zmin
     if nz is not None:
-        z_list = np.linspace(zmin, z, nz+1)
+        z_list = np.linspace(zmin, z, nz + 1)
     if layer_height is not None:
         z_list = np.arange(zmin, z, layer_height)
-        z_list = np.concatenate((z_list,np.array([z])))
+        z_list = np.concatenate((z_list, np.array([z])))
     # bo = BOPAlgo_Builder()
     # bo = BOPAlgo_MakerVolume()
     bo = BOPAlgo_Splitter()
     bo.AddArgument(item)
     for i in z_list:
-        p1, v1 = gp_Pnt(0,0,i), gp_Vec(0, 0, 1)
-        fc1 = make_face(gp_Pln(p1, vec_to_dir(v1)), -plan_len, plan_len, -plan_len, plan_len)
+        p1, v1 = gp_Pnt(0, 0, i), gp_Vec(0, 0, 1)
+        fc1 = make_face(
+            gp_Pln(p1, vec_to_dir(v1)), -plan_len, plan_len, -plan_len, plan_len
+        )
         bo.AddTool(fc1)
     if ny is not None:
         y = ymax - ymin
         y_list = np.linspace(0, y, ny)
         for i in y_list:
-            p1, v1 = gp_Pnt(0,i,0), gp_Vec(0, 1, 0)
-            fc1 = make_face(gp_Pln(p1, vec_to_dir(v1)), -plan_len, plan_len, -plan_len, plan_len)
+            p1, v1 = gp_Pnt(0, i, 0), gp_Vec(0, 1, 0)
+            fc1 = make_face(
+                gp_Pln(p1, vec_to_dir(v1)), -plan_len, plan_len, -plan_len, plan_len
+            )
             bo.AddArgument(fc1)
     if nx is not None:
         x = xmax - xmin
         x_list = np.linspace(0, x, nx)
         for i in x_list:
-            p1, v1 = gp_Pnt(i,0,0), gp_Vec(1, 0, 0)
-            fc1 = make_face(gp_Pln(p1, vec_to_dir(v1)), -plan_len, plan_len, -plan_len, plan_len)
+            p1, v1 = gp_Pnt(i, 0, 0), gp_Vec(1, 0, 0)
+            fc1 = make_face(
+                gp_Pln(p1, vec_to_dir(v1)), -plan_len, plan_len, -plan_len, plan_len
+            )
             bo.AddArgument(fc1)
     bo.Perform()
     top = Topo(bo.Shape())
     geo = sgeom.create_compound(top.solids())
     return geo
 
+
 def get_face_center_of_mass(face: TopoDS_Face, gp_pnt: bool = False) -> tuple:
     """
-     @brief Get the center of mass of a TopoDS_Face. This is useful for determining the center of mass of a face or to get the centre of mass of an object's surface.
-     @param face TopoDS_Face to get the center of mass of
-     @param gp_pnt If True return an gp_Pnt object otherwise a tuple of coordinates.
+    @brief Get the center of mass of a TopoDS_Face. This is useful for determining the center of mass of a face or to get the centre of mass of an object's surface.
+    @param face TopoDS_Face to get the center of mass of
+    @param gp_pnt If True return an gp_Pnt object otherwise a tuple of coordinates.
     """
     props = GProp_GProps()
     brepgprop_SurfaceProperties(face, props)
@@ -141,31 +157,35 @@ def get_face_center_of_mass(face: TopoDS_Face, gp_pnt: bool = False) -> tuple:
     else:
         return face_surf.Coord()
 
+
 def get_occ_bounding_box(shape: TopoDS_Shape) -> tuple:
     """
-     @brief Get bounding box of occupied space of topo shape.
-     @param shape TopoDS_Shape to be searched for occupied space
-     @return bounding box of occupied space in x y z coordinates
+    @brief Get bounding box of occupied space of topo shape.
+    @param shape TopoDS_Shape to be searched for occupied space
+    @return bounding box of occupied space in x y z coordinates
     """
     bbox = Bnd_Box()
     add_bbox = brepbndlib_Add(shape, bbox)
     xmin, ymin, zmin, xmax, ymax, zmax = bbox.Get()
     return xmin, ymin, zmin, xmax, ymax, zmax
 
+
 def topo_explorer(shape: TopoDS_Shape, shape_type: str) -> list:
     """
-     @brief TopoDS Explorer for shape_type. This is a wrapper around TopExp_Explorer to allow more flexibility in the explorer
-     @param shape TopoDS_Shape to be explored.
-     @param shape_type Type of shape e. g. wire face shell solid compound edge
-     @return List of TopoDS_Shape that are explored by shape_type. Example : [ TopoDS_Shape ( " face " ) TopoDS_Shape ( " shell "
+    @brief TopoDS Explorer for shape_type. This is a wrapper around TopExp_Explorer to allow more flexibility in the explorer
+    @param shape TopoDS_Shape to be explored.
+    @param shape_type Type of shape e. g. wire face shell solid compound edge
+    @return List of TopoDS_Shape that are explored by shape_type. Example : [ TopoDS_Shape ( " face " ) TopoDS_Shape ( " shell "
     """
     result = []
-    map_type = {"wire": TopAbs_WIRE,
-                "face": TopAbs_FACE,
-                "shell": TopAbs_SHELL,
-                "solid": TopAbs_SOLID,
-                "compound": TopAbs_COMPOUND,
-                "edge": TopAbs_EDGE}
+    map_type = {
+        "wire": TopAbs_WIRE,
+        "face": TopAbs_FACE,
+        "shell": TopAbs_SHELL,
+        "solid": TopAbs_SOLID,
+        "compound": TopAbs_COMPOUND,
+        "edge": TopAbs_EDGE,
+    }
     explorer = TopExp_Explorer(shape, map_type[shape_type])
     # This method is called by the explorer.
     while explorer.More():
