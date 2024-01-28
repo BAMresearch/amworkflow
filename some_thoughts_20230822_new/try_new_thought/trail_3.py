6d023a4736c401626bafbf3a113919726751fad5
Yuxiang <yuxianghe61@gmail.com>
2024-01-29 00:33:30 +0100

    update

--- some_thoughts_20230822_new/try_new_thought/trail_3.py
+++ some_thoughts_20230822_new/try_new_thought/trail_3.py
@@ -7,6 +7,7 @@ from amworkflow.api import amWorkflow as aw
 from amworkflow.src.geometries.composite_geometry import CreateWallByPointsUpdate
 from amworkflow.src.utils.writer import stl_writer
 
+
 def honeycomb_infill(
     overall_length: float,
     overall_width: float,
@@ -15,6 +16,7 @@ def honeycomb_infill(
     angle: float = 1.1468,
     regular: bool = False,
     side_len: float = None,
+    expand_factor: float = 0,
 ):
     """
     Create honeycomb geometry.
@@ -30,16 +32,26 @@ def honeycomb_infill(
 
     def calculate_lengths_and_widths(angle_length_pair):
         x_rad = np.radians(angle_length_pair[0])
-        equation1 = ((2 * np.cos(x_rad) + 2) * angle_length_pair[1] * honeycomb_num + 1.5 * line_width - overall_length)
-        equation2 = 3 * line_width + 2 * angle_length_pair[1] * np.sin(x_rad) - overall_width
+        equation1 = (
+            (2 * np.cos(x_rad) + 2) * angle_length_pair[1] * honeycomb_num
+            + 1.5 * line_width
+            - overall_length
+        )
+        equation2 = (
+            3 * line_width + 2 * angle_length_pair[1] * np.sin(x_rad) - overall_width
+        )
         return [equation1, equation2]
 
     def create_half_honeycomb(origin, side_length1, side_length2, angle):
         points = np.zeros((5, 2))
         points[0] = origin
-        points[1] = origin + np.array([side_length1 * np.cos(angle), side_length1 * np.sin(angle)])
+        points[1] = origin + np.array(
+            [side_length1 * np.cos(angle), side_length1 * np.sin(angle)]
+        )
         points[2] = points[1] + np.array([side_length2, 0])
-        points[3] = points[2] + np.array([side_length1 * np.cos(angle), -side_length1 * np.sin(angle)])
+        points[3] = points[2] + np.array(
+            [side_length1 * np.cos(angle), -side_length1 * np.sin(angle)]
+        )
         points[4] = points[3] + np.array([side_length2 * 0.5, 0])
         return points
 
@@ -47,8 +59,15 @@ def honeycomb_infill(
         if overall_width is not None:
             overall_length -= int(line_width)
             overall_width -= int(line_width)
-            initial_guesses = [(x, y) for x in range(0, 89, 10) for y in range(1, overall_length, 10)]
-            updated_solutions = {(round(sol[0], 10), round(sol[1], 10)) for guess in initial_guesses for sol in [fsolve(calculate_lengths_and_widths, guess)] if 0 <= sol[0] <= 90 and 0 <= sol[1] <= 150}
+            initial_guesses = [
+                (x, y) for x in range(0, 89, 10) for y in range(1, overall_length, 10)
+            ]
+            updated_solutions = {
+                (round(sol[0], 10), round(sol[1], 10))
+                for guess in initial_guesses
+                for sol in [fsolve(calculate_lengths_and_widths, guess)]
+                if 0 <= sol[0] <= 90 and 0 <= sol[1] <= 150
+            }
             if updated_solutions:
                 ideal_solution = min(updated_solutions, key=lambda x: np.abs(x[0] - 45))
             else:
@@ -56,11 +75,17 @@ def honeycomb_infill(
             length = ideal_solution[1]
             angle = np.radians(ideal_solution[0])
         else:
-            length = ((overall_length - 1.5 * line_width) / (2 * np.cos(angle) + 2) / honeycomb_num)
+            length = (
+                (overall_length - 1.5 * line_width)
+                / (2 * np.cos(angle) + 2)
+                / honeycomb_num
+            )
             overall_width = 3 * line_width + 2 * np.sin(angle) * length
     else:
         length = side_len
-        overall_length = 1.5 * line_width + (2 * np.cos(angle) + 2) * side_len * honeycomb_num
+        overall_length = (
+            1.5 * line_width + (2 * np.cos(angle) + 2) * side_len * honeycomb_num
+        )
         overall_width = 3 * line_width + 2 * np.sin(angle) * length
 
     start_point = np.array([0, line_width * 0.5])
@@ -69,29 +94,45 @@ def honeycomb_infill(
     half_points = np.zeros((int((12 + (honeycomb_num - 1) * 10) / 2), 2))
     half_points[0] = start_point
     for i in range(honeycomb_num):
-        start = start_point + offset + np.array([(2 * np.cos(angle) + 2) * length * i, 0])
+        start = (
+            start_point + offset + np.array([(2 * np.cos(angle) + 2) * length * i, 0])
+        )
         honeycomb_unit = create_half_honeycomb(start, length, length, angle)
         half_points[i * 5 + 1 : i * 5 + 6] = honeycomb_unit
 
     another_half = np.flipud(np.copy(half_points) * [1, -1])
     points = np.concatenate((half_points, another_half), axis=0)
-    outer_points = np.array([[0, -overall_width * 0.5], [overall_length, -overall_width * 0.5], [overall_length, overall_width * 0.5], [0, overall_width * 0.5]])
+    outer_points = np.array(
+        [
+            [0, -overall_width * 0.5],
+            [overall_length, -overall_width * 0.5],
+            [overall_length, overall_width * 0.5],
+            [0, overall_width * 0.5],
+        ]
+    )
     points = np.concatenate((points, outer_points), axis=0)
-
+    if expand_factor != 0:
+        cnt = np.mean(points, axis=0)
+        points[2:-6] = (points[2:-6] - cnt) * (1 + expand_factor) + cnt
     return points
 
 
-th = 11.4  # 5.652
+th = 10  # 5.652
 # ppt = honeycomb_infill(150, 8, 3)
 # ppt = honeycomb_infill(
 #     regular=True, side_len=63.26, angle=np.deg2rad(84.79), honeycomb_num=2, line_width=8
 # )
 ppt = honeycomb_infill(
-    overall_length=700, overall_width=150, line_width=th, honeycomb_num=3
+    overall_length=150,
+    overall_width=150,
+    line_width=th,
+    honeycomb_num=1,
+    expand_factor=0.1,
 )
 print(ppt)
 # Writing to a CSV file
-file_path = "/Users/yuxianghe/Documents/BAM/amworkflow_restructure/beam_honeycomb_700x150x150x11.4"
+# file_path = "/Users/yuxianghe/Documents/BAM/amworkflow_restructure/beam_honeycomb_700x150x150x11.4"
+file_path = "/Users/yuxianghe/Documents/BAM/amworkflow_restructure/cube_honeycomb_150x150x150x10_10pct.csv"
 with open(file_path, "w", newline="") as file:
     writer = csv.writer(file)
 
