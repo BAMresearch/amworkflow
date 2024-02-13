e24c0d6e4218fdf41be85aacf3ad7487d5252cfc
Yuxiang <yuxianghe61@gmail.com>
2024-02-13 12:07:23 +0100

    solve git issue

--- tests/test_simple_geometries.py
+++
@@ -1,7 +0,0 @@
-from OCC.Core.TopoDS import TopoDS_Solid
-from amworkflow.geometry import simple_geometries
-
-
-def test_create_box():
-    box = simple_geometries.create_box(1.0, 2.0, 3.0)
-    assert isinstance(box, TopoDS_Solid)
